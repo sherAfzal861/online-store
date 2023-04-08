958b93c54efe6afda6c5fe06e3a3268f3cfb9c41
sherAfzal861 <95088124+sherAfzal861@users.noreply.github.com>
2023-04-08 15:26:25 +0500

    created a model(product) and a template(category)
    today added two things in project
    1: created a model named product to save the information about products in the stock
    2: and a template to show the products in stock based on category means that when user click on specific category then all of products in that category will be shown to user.

--- app/views.py
+++ app/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+from . models import Product
+from django.db.models import Count
 # Create your views here.
 
 def home(request):
     return render(request, "index.html")
+
+class CategoryView(View):
+    def get(self, request, val):
+        product = Product.objects.filter(category = val)
+        title = Product.objects.filter(category=val).values("title")
+        return render(request, "category.html", locals())
