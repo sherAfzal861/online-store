a84e0147f35671f5a47d53e1196e9298c74b5796
sherAfzal861 <95088124+sherAfzal861@users.noreply.github.com>
2023-05-15 17:09:30 +0500

    profile section and change password section done
    completed the profile section by adding functionality off address button . and adding the update address functionality.
    Then created change password form and related functionaliies

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_view
-from . forms import LoginForm , MyPasswordResetForm
+from . forms import LoginForm , MyPasswordResetForm, MyPasswordChangeForm
 urlpatterns = [
     path("", views.home , name = 'home'),
     path("about/", views.about, name="about"),
@@ -12,11 +12,18 @@ urlpatterns = [
     path("product-detail/<int:pk>", views.ProductDetail.as_view(), name="product-detail"),
     path("category-title/<val>", views.CategoryTitle.as_view(), name="category-title"),
     path("profile/", views.ProfileView.as_view(), name="profile"),
+    path('address/', views.address, name='address'),
+    path('updateAddress/<int:pk>', views.updateAddress.as_view(), name='updateAddress'),
     #login authentication
     path('registration/', views.CustomerRegistrationview.as_view(), name='customerregistration'),
     path('accounts/login/', auth_view.LoginView.as_view(template_name='login.html',
     authentication_form=LoginForm), name='login'),
     path('password-reset/', auth_view.PasswordResetView.as_view
          (template_name='app/password_reset.html', form_class=MyPasswordResetForm),
-         name='password_reset')
+         name='password_reset'),
+    path('passwordchange/', auth_view.PasswordChangeView.as_view(template_name=
+        'changepassword.html', form_class=MyPasswordChangeForm, success_url=
+        '/passwordchangedone'), name='passwordchange'),
+    path('passwordchangedone/', auth_view.PasswordChangeDoneView.as_view(template_name=
+        "passwordchangedone.html"), name = "passwordchangedone")
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import View
-from . models import Product
+from . models import Product, Customer
 from django.db.models import Count
 from . forms import CustomerRegistrationForm, CustomerProfileForm
 from django.contrib import messages
@@ -52,4 +52,44 @@ class ProfileView(View):
         form = CustomerProfileForm()
         return render(request, "profile.html", locals())
     def post(self,request):
+        form = CustomerProfileForm(request.POST)
+        if form.is_valid():
+            user = request.user
+            name = form.cleaned_data['name']
+            locality= form.cleaned_data['locality']
+            city= form.cleaned_data['city']
+            mobile= form.cleaned_data['mobile']
+            state= form.cleaned_data['state']
+            zipcode= form.cleaned_data['zipcode']
+            reg = Customer(user=user, name=name, locality=locality, mobile=mobile, city=city, state=state, zipcode=zipcode)
+            reg.save()
+            messages.success(request, "Congratulations! Profile Save Successfully")
+        else:
+            messages.warning(request, "Invalid Input Data")
         return render(request, "profile.html", locals())
+
+def address(request):
+    add= Customer.objects.filter(user= request.user)
+    return render(request, 'address.html', locals())
+
+class updateAddress(View):
+    def get(self, request, pk):
+        add = Customer.objects.get(pk=pk)
+        form = CustomerProfileForm(instance=add)
+        return render(request, 'updateAddress.html', locals())
+    def post(self, request, pk):
+        form = CustomerProfileForm(request.POST)
+        if form.is_valid():
+            add = Customer.objects.get(pk=pk)
+            add.name = form.cleaned_data['name']
+            add.locality= form.cleaned_data['locality']
+            add.city= form.cleaned_data['city']
+            add.mobile= form.cleaned_data['mobile']
+            add.state= form.cleaned_data['state']
+            add.zipcode= form.cleaned_data['zipcode']            
+            add.save()
+            messages.success(request, "Congratulations! Profile updated Successfully")
+        else:
+            messages.success(request, "Invalid Input Data")
+
+        return redirect("address")
