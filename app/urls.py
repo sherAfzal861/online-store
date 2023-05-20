0208d7ab8e4f8535b0a3bdde91ed9c6b0630a40d
sherAfzal861 <95088124+sherAfzal861@users.noreply.github.com>
2023-05-20 13:33:54 +0500

    logout and password reset functionality
    completed the logout reset section. related changes are done in template folder and urls.py file also add some lines in settings.py file for email recieving

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_view
-from . forms import LoginForm , MyPasswordResetForm, MyPasswordChangeForm
+from . forms import LoginForm , MyPasswordResetForm, MyPasswordChangeForm, MySetPasswordForm
 urlpatterns = [
     path("", views.home , name = 'home'),
     path("about/", views.about, name="about"),
@@ -18,12 +18,17 @@ urlpatterns = [
     path('registration/', views.CustomerRegistrationview.as_view(), name='customerregistration'),
     path('accounts/login/', auth_view.LoginView.as_view(template_name='login.html',
     authentication_form=LoginForm), name='login'),
-    path('password-reset/', auth_view.PasswordResetView.as_view
-         (template_name='app/password_reset.html', form_class=MyPasswordResetForm),
-         name='password_reset'),
+   
     path('passwordchange/', auth_view.PasswordChangeView.as_view(template_name=
         'changepassword.html', form_class=MyPasswordChangeForm, success_url=
         '/passwordchangedone'), name='passwordchange'),
     path('passwordchangedone/', auth_view.PasswordChangeDoneView.as_view(template_name=
-        "passwordchangedone.html"), name = "passwordchangedone")
+        "passwordchangedone.html"), name = "passwordchangedone"),
+    path('logout/', auth_view.LogoutView.as_view(next_page='login'), name='logout'),
+    
+    path('password-reset/', auth_view.PasswordResetView.as_view(template_name='password_reset.html',
+        form_class=MyPasswordResetForm), name='password_reset'),
+    path('password-reset/done/', auth_view.PasswordResetDoneView.as_view(template_name='password_reset__done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_view.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html", form_class=MySetPasswordForm), name='password_reset_confirm'),
+    path('password-reeset-complete/', auth_view.PasswordResetCompleteView.as_view(template_name="password_reset_complete.html"), name='password_reset_complete'),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
